225e7ed37304d90b60b27c742fde9f83e3d80936
Victor Kim <hskim105@gmail.com>
2017-02-10 00:46:32 -0500

    Added abstrcat functions

--- player.py
+++ player.py
@@ -15,7 +15,53 @@ class Player(object):
         self.add = lambda val,rand: (val+rand) % 16
         self.replace = lambda val,rand: rand % 16
         self.skip = lambda val, rand: val
-
+    
+    def add_to(randBit, pos, four_bits):
+        """
+        This function will add the passed random bit to the value in the position
+        
+        Args:
+            randBit (int): current random bit
+            pos (int): position in the four_bit. Has to be [0|1|2|3]
+            four_bit (int[]): the four bit number in the LED
+        
+        Returns:
+            randBit: randBit + four_bit[pos]
+            pos: [0|1|2|3]
+        """
+        randBit += four_bit[pos]
+        return randbit, pos
+        
+    def replace_to(randBit, pos, four_bits):
+        """
+        This function will replace the value in the position with the rand bit
+        
+        Args:
+            randBit (int): current random bit
+            pos (int): position in the four_bit. Has to be [0|1|2|3]
+            four_bit (int[]): the four bit number in the LED
+        
+        Returns:
+            randBit: randBit + four_bit[pos]
+            pos: [0|1|2|3]
+        """
+        four_bit[pos] = randBit
+        return randBit, pos
+        
+    def find_code_digits(randBit):
+        """
+        This function will find if the passed in random bit can
+        add or replace an existing bit to become the code digit
+        
+        Args:
+            randBit (int): current random bit
+        Returns:
+            operation: [self.add | self.replace]
+            selected: [0|1|2|3]
+        """
+        
+    
+    
     def make_decision(self, four_bits, next_randoms, code_digits):
         """
         This function decide next move of the machine player.
@@ -35,4 +81,10 @@ class Player(object):
         operation = self.skip
     	selected = 0
     	#Your Code is Here
+        """
+        Logic:
+        1.  Check if the random bit can be add/replace bits in position 2 | 3
+            If it can, then replace the highest one
+        """
+        if next_randoms[0] == 
     	return operation, selected
