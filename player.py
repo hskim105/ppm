9a65ee13bedd32add5c863e194672b6635d47f71
Victor Kim <hskim105@gmail.com>
2017-02-11 21:19:12 -0500

    Implemented checking code digits

--- player.py
+++ player.py
@@ -16,52 +16,6 @@ class Player(object):
         self.replace = lambda val,rand: rand % 16
         self.skip = lambda val, rand: val
     
-    def add_to(randBit, pos, four_bits):
-        """
-        This function will add the passed random bit to the value in the position
-        
-        Args:
-            randBit (int): current random bit
-            pos (int): position in the four_bit. Has to be [0|1|2|3]
-            four_bit (int[]): the four bit number in the LED
-        
-        Returns:
-            randBit: randBit + four_bit[pos]
-            pos: [0|1|2|3]
-        """
-        randBit += four_bit[pos]
-        return randbit, pos
-        
-    def replace_to(randBit, pos, four_bits):
-        """
-        This function will replace the value in the position with the rand bit
-        
-        Args:
-            randBit (int): current random bit
-            pos (int): position in the four_bit. Has to be [0|1|2|3]
-            four_bit (int[]): the four bit number in the LED
-        
-        Returns:
-            randBit: randBit + four_bit[pos]
-            pos: [0|1|2|3]
-        """
-        four_bit[pos] = randBit
-        return randBit, pos
-        
-    def find_code_digits(randBit):
-        """
-        This function will find if the passed in random bit can
-        add or replace an existing bit to become the code digit
-        
-        Args:
-            randBit (int): current random bit
-        Returns:
-            operation: [self.add | self.replace]
-            selected: [0|1|2|3]
-        """
-        
-    
-    
     def make_decision(self, four_bits, next_randoms, code_digits):
         """
         This function decide next move of the machine player.
@@ -78,13 +32,28 @@ class Player(object):
         	selected: [0|1|2|3]
         """
         
-        operation = self.skip
-    	selected = 0
+
     	#Your Code is Here
         """
         Logic:
         1.  Check if the random bit can be add/replace bits in position 2 | 3
             If it can, then replace the highest one
         """
-        if next_randoms[0] == 
+        
+        if next_randoms[0] == code_digits[0] or next_randoms[0] + four_bits[2] == code_digits[0] or next_randoms[0] == code_digits[1] or next_randoms[0] + four_bits[3] == code_digits[1]:
+            if next_randoms[0] == code_digits[0]:
+                operation = self.replace
+                selected = 2
+            elif next_randoms[0] + four_bits[2] == code_digits[0]:
+                operation = self.add
+                selected = 2
+            elif next_randoms[0] == code_digits[1]:
+                operation = self.replace
+                selected = 3
+            elif next_randoms[0] + four_bits[3] == code_digits[1]:
+                operation = self.add
+                selected = 3
+        else:
+            operation = self.replace
+            selected = 0
     	return operation, selected
