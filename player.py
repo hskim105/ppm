b4ffa035756f875b7094e3965b64b5bb9c6f4530
Victor Kim <hskim105@gmail.com>
2017-02-12 01:05:59 -0500

    Added pseudo code for adjacency

--- player.py
+++ player.py
@@ -15,7 +15,24 @@ class Player(object):
         self.add = lambda val,rand: (val+rand) % 16
         self.replace = lambda val,rand: rand % 16
         self.skip = lambda val, rand: val
-    
+        
+    # def findMaxOpPos(currentMax, newVal, op, pos):
+ #        """
+ #        This function finds the operation and position that will yield the maximum value of points
+ #
+ #        Args:
+ #            currentMax (int): The current max value
+ #            newVal (int): New value that will be compared to the current max
+ #            op: [self.add | self.replace]
+ #            pos (int): [0|1|2|3]
+ #
+ #        Returns:
+ #            newMax (int)
+ #            operation: [self.skip | self.add | self.replace]
+ #            selected: [0|1|2|3]
+ #        """
+ #
+        
     def make_decision(self, four_bits, next_randoms, code_digits):
         """
         This function decide next move of the machine player.
@@ -32,27 +49,100 @@ class Player(object):
         	selected: [0|1|2|3]
         """
         
+        #The random bit
+        randBit = next_randoms[0]
+        
+        #Two code digits placed on position 2 (codeOne) and position 3 (codeTwo)
+        codeOne = code_digits[0]
+        codeTwo = code_digits[1]
+        
+        #The values that are in the array
+        bitOne = four_bits[0]
+        bitTwo = four_bits[1]
+        bitThree = four_bits[2]
+        bitFour = four_bits[3]
+        
         """
         Logic:
-        1.  Check if the random bit can be add/replace bits in position 2 | 3
+        1.  Check if the random bit can add/replace bits in position 2 | 3
             If it can, then replace the highest one
         
         2.  Check all possible adjacency and pick the max value
         """
+        #Checks if the random bit can add/replace bits in position 2 | 3
+        if randBit == codeOne or (randBit + bitThree) == codeOne or randBit == codeTwo or (randBit + bitFour) == codeTwo:
+            #Checks if the random bit is equal to the value of the first code digit (pos 2)
+            if randBit == codeOne:
+                operation = self.replace
+                selected = 2
+            #Checks if the random bit plus the value of the third bit is equal to the first code digit
+            elif randBit + bitThree == codeOne:
+                operation = self.add
+                selected = 2
+            #Checks if the random bit is equal to the value of the second code digit (pos 3)
+            elif randBit == codeTwo:
+                operation = self.replace
+                selected = 3
+            #Checks if the random bit plus the value of the fourth bit is equal to the second code digit
+            elif randBit + bitFour == codeTwo:
+                operation = self.add
+                selected = 3
+                
+        #Checks if there are any adjacency in the list
+        elif randBit in four_bits or (randBit + bitOne) in four_bits or (randBit + bitTwo) in four_bits or (randBit + bitThree) in four_bits or (randBit + bitFour) in four_bits:
+            
+            #Default values. Will be changed after for loop
+            currentMax = 0
+            operation = self.skip
+            selected = 0
+            
+            #Looping through each bit in the four_bits array
+            for index in four_bits:
+                #Checking all possible adjacency at position 0
+                if index == bitOne:
+                    #Check 3 adjacency with replacement
+                    #Check 3 adjacency with addition
+                    
+                    #Check 2 adjacency with replacement
+                    #Check 2 adjacency with addition
+                    
+                    #Check 1 adjacency with replacement
+                    #Check 1 adjacency with addition
+                    print "HI"
+                #Checking all possible adjacency at position 1
+                elif index == bitTwo:
+                    #Check 3 adjacency with replacement
+                    #Check 3 adjacency with addition
+                    
+                    #Check 2 adjacency with replacement
+                    #Check 2 adjacency with addition
+                    
+                    #Check 1 adjacency with replacement
+                    #Check 1 adjacency with addition
+                    print "HI"
+                #Checking all possible adjacency at position 2
+                elif index == bitThree:
+                    #Check 3 adjacency with replacement
+                    #Check 3 adjacency with addition
+                    
+                    #Check 2 adjacency with replacement
+                    #Check 2 adjacency with addition
+                    
+                    #Check 1 adjacency with replacement
+                    #Check 1 adjacency with addition
+                    print "HI"
+                #Checking all possible adjacency at position 3
+                elif index == bitFour:
+                    #Check 3 adjacency with replacement
+                    #Check 3 adjacency with addition
+                    
+                    #Check 2 adjacency with replacement
+                    #Check 2 adjacency with addition
+                    
+                    #Check 1 adjacency with replacement
+                    #Check 1 adjacency with addition
+                    print "HI"
         
-        if next_randoms[0] == code_digits[0] or next_randoms[0] + four_bits[2] == code_digits[0] or next_randoms[0] == code_digits[1] or next_randoms[0] + four_bits[3] == code_digits[1]:
-            if next_randoms[0] == code_digits[0]:
-                operation = self.replace
-                selected = 2
-            elif next_randoms[0] + four_bits[2] == code_digits[0]:
-                operation = self.add
-                selected = 2
-            elif next_randoms[0] == code_digits[1]:
-                operation = self.replace
-                selected = 3
-            elif next_randoms[0] + four_bits[3] == code_digits[1]:
-                operation = self.add
-                selected = 3
         else:
             operation = self.replace
             selected = 0
